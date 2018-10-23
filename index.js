9bac5f9d8848554fd1adecb0d112877e6d0f6621
Natalia Shelley <sorellab@Natalias-MacBook-Pro.local>
2018-10-23 16:40:13 -0600

    adds port

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.set('port', (process.env.PORT || 5000));
 app.get('/', function(req, res){
     res.send("hello world")
 })
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+    });
