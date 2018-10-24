a268404ac7e3eee450bc8f114547288854feb404
Natalia Shelley <sorellab@Natalias-MacBook-Pro.local>
2018-10-24 15:45:41 -0600

    First attempt to crall a web page

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
-
+const request = require('request');
+const rp = require('request-promise');
+var cheerio = require('cheerio')
 const app = express();
+
+var options = {
+    uri: 'https://www.lds.org/?lang=eng',
+    transform: function (body) {
+        return cheerio.load(body);
+    }
+};
 
 app.set('port', (process.env.PORT || 5000));
 app.get('/', function(req, res){
-    res.send("hello world")
+    rp(options)
+    .then(function ($) {
+        var img = $('img[alt="President Nelson"]').attr("src")
+        res.send(img)
+    })
+    .catch(function (err) {
+        // Crawling failed or Cheerio choked...
+        console.log(err)
+    });
+   
 })
 
 app.listen(app.get('port'), function() {
