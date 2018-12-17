4c588ac447e6c368b32a390e580dce9db5e09365
Natalia Shelley <sorellab@Natalias-MacBook-Pro.local>
2018-12-17 14:37:39 -0700

    adds text scraping 3

--- index.js
+++ index.js
@@ -11,11 +11,19 @@ var options = {
     }
 };
 
-var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "Z"]
-var ids = letters.map(function(letter){
-    return 'letter=' + letter
-})
-
+// var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "Z"]
+// var ids = letters.map(function(letter){
+//     return 'letter=' + letter
+// })
+function parseTopic(url){
+    return rp(url)
+    .then(function(html) {
+      return $("#overview > .lumen-content-block:first-child > p").text()
+    })
+    .catch(function(err) {
+      //handle error
+    });
+}
 app.set('port', (process.env.PORT || 5000));
 app.get('/', function(req, res){
     rp(options)
@@ -26,7 +34,8 @@ app.get('/', function(req, res){
         $(item).find(".list--stripped > li > a").each(function(index, link) {
             var topic = {}
             topic.link = $(link).attr("href")
-            topic.title = $(link).text()
+            topic.title = $(link).text();
+            topic.text = parseTopic($(link).attr("href"))
             arr.push(topic)
         })
         obj[$(item).attr("id")] = arr;
